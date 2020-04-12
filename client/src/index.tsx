17034622dcdf58d8679bc61088b85556eb41a7e0
Blas Santomé <bso080601@icloud.com>
2020-04-12 20:48:02 +0200

    Fix remote prisma

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -12,7 +12,8 @@ import {RoleProvider} from "./context/Role";
 
 
 const client = new ApolloClient({
-    uri: 'http://api:4000',
+    //uri: 'http://localhost:4000',
+    uri: 'http://ociofy.es:4000',
 });
 ReactDOM.render(
     <ApolloProvider client={client}>
